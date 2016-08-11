7986bbfc553a8011a7d13541d9fb776748ad96ff
Sagar <sharmasagar@Sagars-MacBook-Pro.local>
2016-08-10 23:29:31 -0400

    Yakozana vs The Undertaker

--- PorkChop/src/porkchop/PorkChop.java
+++ PorkChop/src/porkchop/PorkChop.java
@@ -1,54 +1,77 @@
 
 package porkchop;
 
+import static java.lang.Math.ceil;
+import java.util.Arrays;
+
 
 public class PorkChop {
         // iterative 
-       public static int chop(int value,int [] arr){
+       public static int chop_it(int value,int [] arr){
+           Arrays.sort(arr, 0, arr.length-1);
            int start_indx=0;
            int end_indx=arr.length -1;
-           
-           int start_indx1=1;
-           int end_indx1= 0;
-           
-           int mid_indx=-1;
-           
-           int counter=(int)Math.log(arr.length);
-           int i =0;
-           
+           int mid_indx;
+        
            if (arr.length==0) return -1;
-           else if (arr.length==1){
-               if (arr[0]==value) {
-                   return 1;
-               }
-               else
-               {return -1;}
+           
+           while ((end_indx>start_indx)){
+           mid_indx=(int)(start_indx+end_indx)/2;      
+           if (arr[start_indx]==value){
+               return start_indx;
            }
-           
-           
-           
-           while ((start_indx!=start_indx1 && end_indx!=end_indx1)){
-           start_indx=start_indx1;
-           end_indx=end_indx1;
-           mid_indx=(start_indx+end_indx)/2;
-    
-           i+=1;
-           if (arr[mid_indx]==value){
+           else if (arr[mid_indx]==value){
                return mid_indx;
            }
+           else if(arr[mid_indx]==end_indx){
+               return end_indx;
+           }
            else if (arr[mid_indx]>value){
-               end_indx1=mid_indx-1;
-               mid_indx=-1;
+               start_indx=mid_indx;
            }
            else{
-               start_indx1=mid_indx+1;
-               mid_indx=-1;
+               end_indx=mid_indx;
            }
 
        }
-           return mid_indx;
-           
+           if (arr[end_indx]==value) return end_indx;
+           return -1;
+                   
        }
+       
+       public static int chop_rec(int value, int [] arr){
+           if (arr.length==0) return -1; 
+           int start_pt=0;
+           int end_pt=arr.length-1;
+           int c;
+           
+           int mid_pt=(int) ceil((float)(start_pt+end_pt)/2);
+           
+           if (arr[start_pt]==value)return start_pt;
+           if (arr[mid_pt]==value) return mid_pt;
+           if (arr[end_pt]==value)return end_pt;
+           
+           if (arr[mid_pt]>value){
+               start_pt=mid_pt; 
+               c=mid_pt;
+           }
+           else {
+               end_pt=mid_pt;
+               c=start_pt;
+           }
+           
+           int[] new_arr=new int[end_pt-start_pt];
+
+           for (int j=0;j<new_arr.length;j++){
+               new_arr[j]=arr[start_pt+j+1];
+           }
+           
+           
+           if (chop_rec(value,new_arr)==-1)return -1;
+           else return c+chop_rec(value,new_arr);
+            
+          
+}
 }
        
        
--- PorkChop/src/porkchop/testPorkChop.java
+++ PorkChop/src/porkchop/testPorkChop.java
@@ -6,8 +6,13 @@ public class testPorkChop {
     
     public static void main(String[] args) {
        
-        System.out.println(PorkChop.chop(5,new int[] {1,2,3}));
-        System.out.println(PorkChop.chop(3,new int[] {1,2,3}));
+       // System.out.println(PorkChop.chop_it(3,new int[] {3}));
+       // System.out.println(PorkChop.chop_it(3,new int[] {1,3,2}));
+       // System.out.println(PorkChop.chop_it(8,new int[] {1,3,2,7,8,9,10,15}));
+        
+        System.out.println(PorkChop.chop_rec(3,new int[] {}));
+        System.out.println(PorkChop.chop_rec(3,new int[] {1,3,2}));
+        System.out.println(PorkChop.chop_rec(8,new int[] {1,3,2,7,8,9,10,15}));
         
     }
     
